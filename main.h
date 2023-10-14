61935494b1dcf443a38c240f006b347a60c75d5b
kaouther <kaoutherboutheldja@gmail.com>
2023-10-14 20:52:11 +0100

    Update changes

--- main.h
+++ main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+// Header files
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+// Prototypes for every function in the project
 void remplirTableauAvecEntiersAliaoire(int tab[], int taille, int ValeurMax);
 void TriParSelection(int tab[], int taille);
 void displayArray(int* , int ,char*);
